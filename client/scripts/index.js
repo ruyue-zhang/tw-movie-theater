81ac6ee06cfd21010abdb0fe878e1cbbc2174f5e
lpdft <772346730@qq.com>
2020-01-14 14:02:19 +0800

    [ruyue] feat-1: add search form input

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,19 +1,19 @@
 let BASIC_URL = 'http://127.0.0.1:8888';
 let movieId = '26942674';
 getDetailById();
-searchByKeyWords();
 
 function searchByKeyWords(){
-  //let keyWords = '后天'//document.getElementsByClassName('search').value;
-  ajax({
-    url: BASIC_URL + "/v2/movie/search?q=后天",
-    method: "GET",
-    success: function(responseText) {
-      console.log(responseText);
-    }, 
-  });
+  let keyWords = document.getElementsByClassName('search-box')[0].value;
+  console.log(keyWords);
+  // ajax({
+  //   url: BASIC_URL + "/v2/movie/search?q=" + keyWords,
+  //   method: "GET",
+  //   success: function(responseText) {
+  //     console.log(responseText);
+  //   }, 
+  // });
 }
-  
+
 function getDetailById() {
   ajax({
     url: BASIC_URL + "/v2/movie/subject/" + movieId,
