6ae78d5c6f7d52c9f75db5fcb81b4ff178ceab0b
mizhidan <dj9824@126.com>
2020-01-14 12:16:15 +0800

    [danjie] feat-2: get movie info from ajax.

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,12 +1,12 @@
 let BASIC_URL = 'http://127.0.0.1:8888';
-let movieId = '26942674';
+let movieId = 'top250';
 
 function getAllData() {
   ajax({
-      url: BASIC_URL + '/v2/movie/subject/' + movieId,
+      url: BASIC_URL + '/v2/movie/' + movieId + '?apikey=',
       method: "GET",
       data: {
-        apikey: '0df993c66c0c636e29ecbb5344252a4a',
+        apikey: '0df993c66c0c636e29ecbb5344252a4a&start=0&count=250',
       },
       success: function(responseText) {
       }, 
