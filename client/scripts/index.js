5b9617d747d1dd2ca9d136d5c6d97c503696e3fd
lpdft <772346730@qq.com>
2020-01-14 14:48:30 +0800

    [ruyue] feat-1: add main-page nav logo

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -2,7 +2,6 @@ let BASIC_URL = 'http://127.0.0.1:8888';
 let movieId = '26942674';
 getDetailById();
 
-getAllData();
 function searchByKeyWords(){
   let keyWords = document.getElementsByClassName('search-box')[0].value;
   console.log(keyWords);
