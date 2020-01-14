724b4dddefd98bbc5b29c106b52ff092fdf973bf
lpdft <772346730@qq.com>
2020-01-14 13:06:45 +0800

    [ruyue] feat-1: add searchByKeyWords function

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,10 +1,30 @@
 let BASIC_URL = 'http://127.0.0.1:8888';
-let movieId = 'top250';
+let movieId = '26942674';
+getDetailById();
+searchByKeyWords();
 
-ajax({
+function searchByKeyWords(){
+  //let keyWords = '后天'//document.getElementsByClassName('search').value;
+  ajax({
+    url: BASIC_URL + "/v2/movie/search?q=后天",
+    method: "GET",
+    success: function(responseText) {
+      console.log(responseText);
+    }, 
+  });
+}
+  
+function getDetailById() {
+  ajax({
     url: BASIC_URL + "/v2/movie/subject/" + movieId,
     method: "GET",
     success: function(responseText) {
       console.log(responseText);
     }, 
-});
+  });
+}
+
+
+
+
+
