1d95b2aa820d8c76a244da13f3b1a3a7120351b1
mizhidan <dj9824@126.com>
2020-01-14 15:35:51 +0800

    [danjie] feat-2: side navbar inner

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,23 +1,63 @@
 let BASIC_URL = 'http://127.0.0.1:8888';
-let movieId = '26942674';
-getDetailById();
-searchByKeyWords();
+var movieId = '26942674';
+const top250 = 'top250'
+ getDetailById();
+ searchByKeyWords();
 
 function getAllData() {
   ajax({
-      url: BASIC_URL + '/v2/movie/' + movieId,
+      url: BASIC_URL + '/v2/movie/' + top250,
       method: "GET", 
       success: function(responseText) {
-        mainPage(responseText);
+         mainPage(responseText);
       }, 
   })
 }
 
 function mainPage(data) {
-  sortGenres(data);
+  getGenres(data.subjects);
+  getMovies(data.subjects);
+}
+
+function getGenres(data) {
+  var tempArr = [];
+  var genArr = [];
+  for(index = 0; index < data.count; ++index) {
+    tempArr.push(data[index].genres);
+  }
+  tempArr.forEach((tItem,index) => {
+    tItem.forEach((innerItem,index) => {
+      genArr.push(innerItem);
+    });
+  })
+  sortGenres(genArr);
 }
 
 function sortGenres(data) {
+  let tempArr = data.sort();
+  let resArr = [];
+  for (let index = 0; index < data.length; ++index) {
+    if(tempArr[index] === tempArr[index+1]) {
+      continue
+    } else {
+      resArr.push(tempArr[index]);
+    }
+  }
+  addResList(resArr);
+}
+
+function addResList(res) {
+  let typeList = document.getElementsByClassName("type-list")[0];
+  for (let index = 0; index < res.length; ++index) {
+    let resItem = document.createElement("li");
+    resItem.setAttribute("class","movie-type");
+    let resname = `${res[index]}`;
+    resItem.innerHTML = resname;
+    typeList.appendChild(resItem);
+  }
+}
+
+function getMovies(data) {
 
 }
 
