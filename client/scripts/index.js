250117381e13156d1172d45d42dcea3427c4327b
mizhidan <dj9824@126.com>
2020-01-15 14:44:32 +0800

    [danjie] feat-2: while click side navbar display corresponding movies

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,7 +1,8 @@
 let BASIC_URL = 'http://127.0.0.1:8888';
 var movieId = '26942674';
-const top250 = 'top250'
- searchByKeyWords();
+const top250 = 'top250';
+var movieDatas = [];
+//  searchByKeyWords();
 
 function getAllData() {
   ajax({
@@ -15,17 +16,18 @@ function getAllData() {
 
 function mainPage(data) {
   getGenres(data.subjects);
-  getMovies(data.subjects);
+  addMoviesInfo(data.subjects);
 }
 
 function getGenres(data) {
+  movieDatas = data;
   var tempArr = [];
   var genArr = [];
-  for(index = 0; index < data.count; ++index) {
+  for(index = 0; index < data.length; ++index) {
     tempArr.push(data[index].genres);
   }
-  tempArr.forEach((tItem,index) => {
-    tItem.forEach((innerItem,index) => {
+  tempArr.forEach((tItem) => {
+    tItem.forEach((innerItem) => {
       genArr.push(innerItem);
     });
   })
@@ -37,7 +39,7 @@ function sortGenres(data) {
   let resArr = [];
   for (let index = 0; index < data.length; ++index) {
     if(tempArr[index] === tempArr[index+1]) {
-      continue
+      continue;
     } else {
       resArr.push(tempArr[index]);
     }
@@ -50,17 +52,85 @@ function addResList(res) {
   for (let index = 0; index < res.length; ++index) {
     let resItem = document.createElement("li");
     resItem.setAttribute("class","movie-type");
+    resItem.setAttribute("id",`${res[index]}`);
     let resname = `${res[index]}`;
     resItem.innerHTML = resname;
     typeList.appendChild(resItem);
   }
+  typeList.addEventListener("click",function(event) {
+    filterMovies(event.target.id);
+  })
 }
 
-function getMovies(data) {
-
+function filterMovies(resId) {
+  let filterArr = [];
+  movieDatas.forEach((item) => {
+    if(item.genres.indexOf(resId) > -1) {
+      filterArr.push(item);
+    }
+  })
+  changeChecked(resId);
+  addMoviesInfo(filterArr);
 }
-let movieId = '26942674';
-getDetailById();
+
+function changeChecked(resId) {
+  let typeList = document.getElementsByClassName("movie-type");
+  let checkedGen = document.getElementById(resId);
+  for (let index = 0; index < typeList.length; ++index) {
+    typeList[index].style.color = 'black';
+  }
+  checkedGen.style.color = 'rgb(253, 112, 18)';
+}
+
+var movieList = document.getElementsByClassName("movie-list")[0];
+
+function addMoviesInfo(data) {
+  movieList.innerHTML = '';
+    for(let rowCount = 0; rowCount < (data.length / 3); ++rowCount) {
+      let movieRow = document.createElement("tr");
+      movieRow.setAttribute("class","movie-row");
+      for(let colCount = 0; colCount < 3; ++colCount) {
+        if(rowCount * 3 + colCount === 100 || data.length <= colCount) {
+          break;
+        }
+        let movieCard = document.createElement("td");
+        movieCard.setAttribute("class","movie-info");
+        let index = rowCount * 3 + colCount;
+        let movieInfos = document.createElement("div");
+        let movieCover = document.createElement("img");
+        let movieNameAndYear = document.createElement("span");
+        let moviePubDate = document.createElement("span");
+        let movieDuration = document.createElement("span");
+        let movieGenres = document.createElement("span");
+        let movieScore = document.createElement("span");
+
+        movieInfos.setAttribute("class","movie-infos");
+        movieCover.setAttribute("class","movie-cover");
+        movieCover.setAttribute("src",`${data[index].images.small}`);
+        movieNameAndYear.setAttribute("class","infos");
+        moviePubDate.setAttribute("class","infos");
+        movieGenres.setAttribute("class","infos");
+        movieDuration.setAttribute("class","infos");
+        movieScore.setAttribute("class","movie-rate");
+
+        movieNameAndYear.innerHTML = `${data[index].title}/${data[index].year}`;
+        moviePubDate.innerHTML = `${data[index].pubdates.join("/")}`;
+        movieDuration.innerHTML = `${data[index].durations.join("/")}`;
+        movieGenres.innerHTML = `${data[index].genres.join("/")}`;
+        movieScore.innerHTML = `${data[index].rating.average}/10`;
+
+        movieInfos.appendChild(movieNameAndYear);
+        movieInfos.appendChild(moviePubDate);
+        movieInfos.appendChild(movieGenres);
+        movieInfos.appendChild(movieScore);
+        movieCard.appendChild(movieCover);
+        movieCard.appendChild(movieInfos);
+        movieRow.appendChild(movieCard);
+      }
+      movieList.appendChild(movieRow);
+    }
+}
+// getDetailById();
 
 getAllData();
 function searchByKeyWords(){
@@ -80,7 +150,7 @@ function getDetailById() {
     url: BASIC_URL + "/v2/movie/subject/" + movieId,
     method: "GET",
     success: function(responseText) {
-      console.log(responseText);
+    console.log(responseText);
     }, 
   });
 }
