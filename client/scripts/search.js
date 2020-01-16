532f8da27d63a94cebf5375b6ce84d0f334c1f3f
lpdft <772346730@qq.com>
2020-01-16 15:31:52 +0800

    [ruyue] feat-5: add background-img and movie informatons

--- client/scripts/search.js
+++ client/scripts/search.js
@@ -5,14 +5,33 @@ getSearchResult();
 let resultList = '';
 function getSearchResult() {
   moviesIdArr.forEach(value => {
-    //console.log(value);
     ajax({
       url: BASIC_URL + "/v2/movie/subject/" + value,
       method: "GET",
       success: function(responseText) {
-        //renderSearchReaults(responseText);
+        console.log(responseText);
+        renderSearchReaults(responseText);
       }, 
     });
   })
 }
 
+function renderSearchReaults(data) {
+  document.getElementsByClassName('result-list')[0].innerHTML += `
+  <li>
+    <div class="movie-infomation">
+      <p class="title">${data.title} - ${data.original_title}(${data.pubdates[0].substring(0,4)})</p>
+      <div class='poster' style="background-img:url(${data.images.small})"></div>
+      <div class="info">
+        <p>导演：${data.directors[0].name}</p>
+        <p>主演：${data.casts.map(value=>value.name).join('/')}</p>
+        <p>类型：${data.genres.join('/')}</p>
+        <p>制片国家/地区：${data.countries}</p>
+        <p>语言：${data.languages.join('/')}</p>
+        <p>片长：${data.durations}</p>
+        <p>上映日期：${data.pubdate}</p>
+        <p>豆瓣评分：${data.rating.average}</p>
+      </div>
+    </div>
+  </li>`;
+}
