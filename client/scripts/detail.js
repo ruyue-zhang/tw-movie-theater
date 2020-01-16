d630bdf8b302762f4b9865c4fef5b43e37aa4f95
lpdft <772346730@qq.com>
2020-01-16 12:59:02 +0800

    [ruyue] feat-5: get infomation by id in search.js

--- client/scripts/detail.js
+++ client/scripts/detail.js
@@ -1,3 +1,4 @@
+let BASIC_URL = 'http://127.0.0.1:8888';
 renderPage();
 
 function renderPage() {
--- client/scripts/search.js
+++ client/scripts/search.js
@@ -1,2 +1,18 @@
+let BASIC_URL = 'http://127.0.0.1:8888';
 let moviesIdArr =  window.location.search.substring(1).split('=')[1].split('&');
 console.log(moviesIdArr);
+getSearchResult();
+let resultList = '';
+function getSearchResult() {
+  moviesIdArr.forEach(value => {
+    //console.log(value);
+    ajax({
+      url: BASIC_URL + "/v2/movie/subject/" + value,
+      method: "GET",
+      success: function(responseText) {
+        //renderSearchReaults(responseText);
+      }, 
+    });
+  })
+}
+
